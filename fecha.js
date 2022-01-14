e26935f59dfa8098f3749677600a17203588124a
asdaf <asdaf5678@gmail.com>
2022-01-14 19:06:31 +0100

    cambiado el estilo del texto de la fecha

--- fecha.js
+++ fecha.js
@@ -2,46 +2,9 @@ n =  new Date();
 y = n.getFullYear();
 m = n.getMonth() + 1;
 d = n.getDate();
-switch (m) {
-    case 1:
-        m="enero"
-        break;
-    case 2:
-        m="febrero"
-        break;
-    case 3:
-        m="marzo"
-        break;
-    case 4:
-        m="abril"
-        break;
-    case 5:
-        m="mayo"
-        break;
-    case 6:
-        m="junio"
-        break;
-    case 7:
-        m="julio"
-        break;
-    case 8:
-        m="agosto"
-        break;
-    case 9:
-        m="septiembre"
-        break;
-    case 10:
-        m="octubre"
-        break;
-    case 11:
-        m="noviembre"
-        break;
-    case 12:
-        m="diciembre"
-        break;
-    default:
-        m="NaN"
-        break;
-}
-
-document.getElementById("date").innerHTML = d + " de " + m + " del " + y;
+patata=["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
+pipo=["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"]
+m=patata[m-1]
+s=n.getDay()
+s=pipo[s]
+document.getElementById("date").innerHTML =s+" "+ d + " de " + m + " del " + y;
