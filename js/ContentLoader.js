0fb5c69912fe911979231227714ae772c2753801
robertobobby1 <roberto.millamartinez@hotmail.com>
2022-01-19 17:43:04 +0100

    refactor para movil parte 2

--- js/ContentLoader.js
+++ js/ContentLoader.js
@@ -8,6 +8,11 @@ function loadContent(contentFile)
     content.html(ajaxContent.responseText)
 
     //movil
+    var ajaxContentPhone = new XMLHttpRequest();
+    ajaxContentPhone.open("GET", "content/movil/" + contentFile, false)
+    ajaxContentPhone.send();
+    var contentPhone = $("#contentPhone");
+    contentPhone.html(ajaxContentPhone.responseText)
 
 }
 
@@ -27,9 +32,14 @@ function setDate()
 
 function SetMenuBar(id)
 {
+    // Ordenador
     let menuBar = $("#" + id);
     menuBar.removeClass();
     menuBar.addClass("boton_superior_actual");
+
+    // Movil
+    let dropdown = document.getElementById("myDropdown");
+    dropdown.children[0].classList.add("selected");
 }
 
 function change_style(actual)
@@ -41,22 +51,83 @@ function change_style(actual)
     document.getElementById(actual).classList.add("boton_actual")
 }
 
-function change_content(actual){
+function change_content(actual)
+{
+    // loads new content
+    loadContent(actual+"Content.html")
+
+    // changes pc html, css part
+    changePCContent(actual);
+
+    //changes phone html, css part
+    changePhoneContent(actual);
+
+    //changes URL for better navigability
+    //changeURL(actual);
+}
+
+function swapMailSubjects(actual){
+    var botonera= $("#botonera");
+    if (actual == "correo" )botonera.css("display","none")
+    else{
+        botonera.css("display","block")
+    }
+}
+
+function changePCContent(actual)
+{
     var botones = ["anuncios","evaluacion","correo","recursos","chat","notas","grupos"]
     botones.forEach(function (i){
         document.getElementById(i).classList.remove("boton_superior_actual")
         document.getElementById(i).classList.add("boton_superior")
     })
     document.getElementById(actual).classList.add("boton_superior_actual")
-    loadContent(actual+"Content.html")
     swapMailSubjects(actual)
 }
 
-function swapMailSubjects(actual){
-
-    var botonera= $("#botonera");
-    if (actual == "correo" )botonera.css("display","none")
-    else{
-        botonera.css("display","block")
+function changePhoneContent(actual)
+{
+    let dropdown = document.getElementById("myDropdown");
+    for(let i =  0; i < dropdown.children.length ; i++) {
+        // UNSELECT ALL
+        if (dropdown.children[i].classList.contains("selected")) {
+            dropdown.children[i].classList.remove("selected");
+        }
+        // SELECT ACTIVE MENU BUTTON
+        if (dropdown.children[i].attributes["data-id"].nodeValue === actual) {
+            dropdown.children[i].classList.add("selected");
+        }
     }
+    // CLOSE DROPDOWN
+    toggleDropDown();
+
+}
+
+function toggleDropDown()
+{
+    let dropDown = document.getElementById("myDropdown");
+    dropDown.classList.toggle("show");
+    // DROPDOWN CLOSED RETURN;
+    if (!dropDown.classList.contains("show")) {
+        $("#contentPhone").css("margin-top", "10px");
+        return;
+    }
+
+    // MOVE CONTENT DOWN
+    let height = dropDown.offsetHeight + 10;
+    $("#contentPhone").css("margin-top", height + "px");
+
+}
+
+function changeURL(actual)
+{
+    window.history.pushState("", "", '/DCU/' + actual);
+    debugger;
+    this.window.onbeforeunload = (event) =>  beforeReload();
+}
+
+function beforeReload()
+{
+    debugger;
+    return "WTF MA NIGGA"
 }
